c43c0515d586bf79dee42a063ec430a02897955d
haddercone <44robin.rs@gmail.com>
2023-02-11 21:20:41 +0530

    search background changed

--- Session-11/code/tailwind.config.js
+++ Session-11/code/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "search-bg":
-          "linear-gradient(45deg, rgba(0,0,0,0.4),rgba(255,255,255,.4)), url('https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')",
+          "linear-gradient(45deg, rgba(0,0,0,0.7),rgba(255,255,255,.4)), url('https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')",
       },
     },
   },
